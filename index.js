cd58fd775a271b7c82a881a912be8709bde948dc
sbb3391 <sbb33391@gmail.com>
2021-05-15 02:20:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+  let a = [];
+  for(let i  = 0; i < array.length; i++) {
+    a.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return a;
+}
+
+function countDown(integer) {
+  while(integer >= 0) {
+    console.log(integer);
+    integer -= 1;
+  }
+}
